580ee36fac7682036fdb52feb22c9ed0f0ff5482
doniahusien <doniahusien@gmail.com>
2025-01-28 12:43:29 +0200

    fix nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
     
     return (
         <>
-            <div className='flex flex-wrap flex-row justify-around items-center  p-6  md:p-8 bg-transparent sticky top-0 z-50 w-full'>
+            <div className='flex flex-wrap flex-row justify-around items-center  p-6  md:p-8 bg-transparent absolute top-0 z-50 w-full'>
                 <div className=''>
                     <Image src={logo} alt="logo" width={240} height={100}/>
                 </div>
