0221f2bfaecb5726d954a5e8db7a34679c639aee
doniahusien <doniahusien@gmail.com>
2025-01-29 18:56:18 +0200

    patients page

--- src/app/patient/page.jsx
+++ src/app/patient/page.jsx
@@ -2,20 +2,21 @@
 
 import React from 'react'
 import HeaderSection from '@/components/ui/HeaderSection/HeaderSection'
-import img from '../../../public/images/patient/patientHeader.png'
+import patientsData from '@/data/patientsData'
+import PatientList from '@/components/patient/PatientList'
 const patientPage = () => {
   return (
     <div className=''>
     <HeaderSection imgURL='/images/patient/patientHeader.png' textDir='center' title="Appropriate Patients" subtitle='Let’s Find ' />
-    <div className='flex flex-col gap-5 mt-10 px-16'>
-      {/*
-        dentalProducts.map(i => (
-          <div key={i.category} className='flex flex-col mb-12'>
-            <h1 className='text-4xl'>{i.category}</h1>
-            <ProductList products={i.products} />
+    <div className='flex flex-col items-center mt-10  '>
+      {
+        patientsData.map(patientItem => (
+          <div key={patientItem.category} className='flex flex-col mb-24 justify-center w-5/6'>
+            <h1 className='text-4xl'>{patientItem.category}</h1>
+            <PatientList patients={patientItem.patients} />
           </div>
         ))
-  */}
+  }
     </div>
     
   </div>
