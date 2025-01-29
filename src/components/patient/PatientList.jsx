955934b2dde8070f5bfa2859e9767ce260de2a5e
doniahusien <doniahusien@gmail.com>
2025-01-29 18:50:57 +0200

    create patient list

---
+++ src/components/patient/PatientList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PatientCard from './PatientCard'
+const PatientList = ({patients}) => {
+  return (
+<div className=' '>
+            <div className="flex flex-row flex-wrap justify-center pt-8 gap-5 relative w-full ">
+                
+                    {patients.map((patient, i) => (
+                        
+                            <PatientCard patient={patient} key={i} />
+                
+                    ))}
+          
+            </div>
+        </div>
+  )
+}
+
+export default PatientList
