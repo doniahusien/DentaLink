d24949b2409db2834d017f3d8ee4cd973010bb26
doniahusien <doniahusien@gmail.com>
2025-01-29 22:02:32 +0200

    seperate links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,22 @@
 'use client'
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
-import logo from '../../public/logo.svg'
+import Logo from './ui/Logo'
 import Button from './ui/Button/Button'
 import { usePathname } from 'next/navigation'
+import mainLinks from '@/data/mainlinks'
+
+
 const Header = () => {
-    
     const pathname = usePathname()
-    const navLinks = [
-        { label: "Home", href: "/" },
-        { label: "About", href: "/about" },
-        { label: "Patient", href: "/patient" },
-        { label: "Market", href: "/market" },
-        { label: "Contact Us", href: "/contact" },
-    ]
-    
     return (
         <>
             <div className='flex flex-wrap flex-row justify-around items-center  p-6  md:p-8 bg-transparent absolute top-0 left-0 z-10 w-full'>
-                <div className=''>
-                    <Image src={logo} alt="logo" width={240} height={100}/>
-                </div>
+            <Logo />
 
                 <div>
                     <ul className='flex flex-row gap-8 pt-4 '>
-                        {navLinks.map((link => (
+                        {mainLinks.map((link => (
                             <li key={link.label}>
                                 <Link href={link.href} style={{fontSize:'20px', color: pathname == link.href ? '#247CFF' : '#1E1E1E'}}>{link.label}</Link>
                             </li>
