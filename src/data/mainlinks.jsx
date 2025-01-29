1f3b56f34aeebf05f20739c6d0359d3c79b0e254
doniahusien <doniahusien@gmail.com>
2025-01-29 22:02:57 +0200

    links and routes

---
+++ src/data/mainlinks.jsx
@@ -0,0 +1,8 @@
+const mainLinks = [
+    { label: "Home", href: "/" },
+    { label: "About", href: "/about" },
+    { label: "Patient", href: "/patient" },
+    { label: "Market", href: "/market" },
+    { label: "Contact Us", href: "/contact" },
+]
+export default mainLinks;
